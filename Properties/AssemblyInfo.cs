96a0eb2aa8974e9df11f61b8cb413d66f546f651
Frederick Pearson <fpearson@cloudstrucc.com>
2022-06-23 03:15:11 -0400

    'test'

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CNSC_ADFS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CNSC_ADFS")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38f43fd2-ad1d-4234-8a01-e11c167b8914")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+
+
+
+[assembly: OwinStartupAttribute(typeof(CS_ADFS.Startup))]
+namespace CS_ADFS
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth1(IAppBuilder app)
+
+        {
+
+            app.UseCookieAuthentication(
+
+            new CookieAuthenticationOptions
+
+            {
+
+                AuthenticationType = WsFederationAuthenticationDefaults.AuthenticationType
+
+            });
+
+            app.UseWsFederationAuthentication(
+
+            new WsFederationAuthenticationOptions
+
+            {
+
+                MetadataAddress = ConfigurationManager.AppSettings["ida:ADFSMetadata"],
+
+                Wtrealm = ConfigurationManager.AppSettings["ida:Wtrealm"]
+
+            });
+
+
+
+            app.SetDefaultSignInAsAuthenticationType(WsFederationAuthenticationDefaults.AuthenticationType);
+
+        }
+    }
+}
